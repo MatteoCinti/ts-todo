248006a65f7bf6afe414f477f4a828b8f004d167
MatteoCinti <matteocinti@hotmail.com>
2022-01-11 21:06:27 +0100

    deployed version state fixes

--- server/app.ts
+++ server/app.ts
@@ -114,7 +114,7 @@ io.on('connection', (socket) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client', 'build')));
 
-  app.get('/^(?!/api).+/', (req, res) => {
+  app.get('/app', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
